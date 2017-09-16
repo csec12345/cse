ecde2a0c9b3385c77ce422342e0d8581ab437ca7
csec12345 <32008505+csec12345@users.noreply.github.com>
2017-09-16 00:47:22 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,25 @@
-import java.util.date;
-public class main{
-public static void main(String[]args){
-    StudentGroup st=new StudentGroup(10);
-    Date d=new Date(1997,18,8);
-Student s=new Student(10,"xyz",a,80.3);
-st.addFirst(s);
-System.out.println(st.getStudent(0).getid());
+import java.util.Date;
 
-}
+public class Main {
+
+	public static void main(String[] args) {
+
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+		StudentGroup 2 = new StudentGroup(5);
+
+ students[] s = new students[5];
+ for(i=0 i<5; i++)
+{
+  s[i]=new student("
+        Student a = new Student(1,"abc",new Date(1996,07,11),63.568);
+		Student b = new Student(2,"xya",new Date(1996,06,06),63.568);
+		Student c = new Student(3,"see",new Date(1996,02,05),63.568);
+		Student d = new Student(4,"you",new Date(1996,05,17),63.568);
+		Student e = new Student(5,"rav",new Date(1996,02,10),63.568);");
+
+
+	}
+
 }
