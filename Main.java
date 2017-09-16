600ee25848c5a9908453832c641b4050206d3899
csec12345 <32008505+csec12345@users.noreply.github.com>
2017-09-16 01:04:00 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -10,14 +10,14 @@ public class Main {
 		StudentGroup 2 = new StudentGroup(5);
 
  students[] s = new students[5];
- for(i=0 i<5; i++)
+ for(i=0;i<5;i++)
 {
   s[i]=new student("
         Student a = new Student(1,"abc",new Date(1996,07,11),63.568);
 		Student b = new Student(2,"xya",new Date(1996,06,06),63.568);
 		Student c = new Student(3,"see",new Date(1996,02,05),63.568);
 		Student d = new Student(4,"you",new Date(1996,05,17),63.568);
-		Student e = new Student(5,"rav",new Date(1996,02,10),63.568);");
+		Student e = new Student(5,"rav",new Date(1996,02,10),63.568);
 
 
 	}
