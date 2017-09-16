de0891d0be0848b6349ebba6b97370ca43933ebf
csec12345 <32008505+csec12345@users.noreply.github.com>
2017-09-16 00:19:59 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.date;
+public class main{
+public static void main(String[]args){
+    StudentGroup st=new StudentGroup(10);
+    Date d=new Date(1997,18,8);
+Student s=new Student(10,"xyz",a,80.3);
+st.addFirst(s);
+System.out.println(st.getStudent(0).getid());
+
+}
+}
